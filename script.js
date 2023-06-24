386bd8309c38c365da1dca0de606d75a94b37247
ShanksSU <87661487+ShanksSU@users.noreply.github.com>
2023-06-24 20:55:57 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+async function play(name) {
+    console.log(name);
+    const buffer = await getBuffer(name + ".aac");
+    buffer && playAudio(buffer);
+}
+const audioContext = new AudioContext();
+const playAudio = function (buffer) {
+    const source = audioContext.createBufferSource();
+    source.buffer = buffer;
+    source.connect(audioContext.destination);
+    source.start();
+};
+
+const getBuffer = function (url) {
+    const request = new XMLHttpRequest();
+    return new Promise((resolve, reject) => {
+        request.open("GET", url, true);
+        request.responseType = "arraybuffer";
+        request.onload = () => {
+        audioContext.decodeAudioData(request.response, (buffer) =>
+            buffer ? resolve(buffer) : reject("decoding error")
+        );
+        };
+        request.onerror = (error) => reject(error);
+        request.send();
+    });
+};
+
+let checkboxes = Array.from(document.querySelectorAll('input[type="checkbox"]'));
+
+checkboxes.forEach(checkbox => {
+    checkbox.onclick = function() {
+        if (this.checked) {
+            this.disabled = true;
+        }
+    }
+});
+
+async function handleReset(name) {
+    console.log(name);
+    checkboxes.forEach(checkbox => {
+        checkbox.disabled = false;
+        checkbox.checked = false;
+    });
+    await play(name);
+}
