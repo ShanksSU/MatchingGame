68474ff7b71910e22399006cc77e9a184c2a5195
ShanksSU <87661487+ShanksSU@users.noreply.github.com>
2023-06-28 19:23:16 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,46 +1,223 @@
-let checkboxes = Array.from(document.querySelectorAll('input[type="checkbox"]'));
+const selectors = {
+    boardContainer: document.querySelector('.board-container'),
+    board: document.querySelector('.boardSet'),
+    moves: document.querySelector('.moves'),
+    timer: document.querySelector('.timer'),
+    start: document.querySelector('.start-btn'),
+    reset: document.querySelector('.reset-btn'),
+    modeBtn: document.querySelector('.mode-btn'),
+    dimensionSelect: document.querySelector('#dimension-select'),
+    win: document.querySelector('.win')
+};
 
-checkboxes.forEach(checkbox => {
-    checkbox.onclick = function() {
-        if (this.checked) {
-            this.disabled = true;
-        }
+const state = {
+    gameStarted: false,
+    flippedCards: 0,
+    totalFlips: 0,
+    totalTime: 0,
+    loop: null,
+    oldDimensions: 4,
+    modeNum: 0,
+    ismodeEnabled: false
+};
+
+const resetGameState = () => {
+    state.gameStarted = false;
+    state.totalFlips = 0;
+    state.totalTime = 0;
+    state.oldDimensions = 4;
+    clearInterval(state.loop);
+};
+
+const shuffleArray = array => {
+    const clonedArray = [...array];
+
+    for (let i = clonedArray.length - 1; i > 0; i--) {
+        const randomIndex = Math.floor(Math.random() * (i + 1));
+        const original = clonedArray[i];
+
+        clonedArray[i] = clonedArray[randomIndex];
+        clonedArray[randomIndex] = original;
     }
-});
 
-async function handleReset(name) {
-    console.log(name);
-    checkboxes.forEach(checkbox => {
-        checkbox.disabled = false;
-        checkbox.checked = false;
-    });
-    await play(name);
-}
-
-async function play(name) {
-    const buffer = await getBuffer(name + ".aac");
-    buffer && playAudio(buffer);
-}
-
-const audioContext = new AudioContext();
-const playAudio = function (buffer) {
-    const source = audioContext.createBufferSource();
-    source.buffer = buffer;
-    source.connect(audioContext.destination);
-    source.start();
+    return clonedArray;
 };
 
-const getBuffer = function (url) {
-    const request = new XMLHttpRequest();
-    return new Promise((resolve, reject) => {
-        request.open("GET", url, true);
-        request.responseType = "arraybuffer";
-        request.onload = () => {
-        audioContext.decodeAudioData(request.response, (buffer) =>
-            buffer ? resolve(buffer) : reject("decoding error")
-        );
-        };
-        request.onerror = (error) => reject(error);
-        request.send();
+const pickRandomElements = (array, items) => {
+    const clonedArray = [...array];
+    const randomPicks = [];
+
+    for (let i = 0; i < items; i++) {
+        const randomIndex = Math.floor(Math.random() * clonedArray.length);
+
+        randomPicks.push(clonedArray[randomIndex]);
+        clonedArray.splice(randomIndex, 1);
+    }
+
+    return randomPicks;
+};
+
+
+const setupGameBoard = () => {
+    state.oldDimensions = parseInt(selectors.dimensionSelect.value);
+    let dimensions = state.oldDimensions;
+    if (dimensions % 2 !== 0) {
+        throw new Error("The dimension of the board must be an even number.");
+    }
+
+    const totalImages = 12;
+    const imageUrls = Array.from({ length: totalImages }, (_, i) => `img/anime/${String(i + 1).padStart(2, '0')}.jpg`);
+
+    const totalBgImages = 3;
+    const bgImage = `img/card/${(Math.floor(Math.random() * totalBgImages) + 1).toString().padStart(2, '0')}.jpg`;
+
+    if (imageUrls.length < dimensions * dimensions / 2) {
+        const repeatImageUrls = [];
+
+        for (let i = 0; i < dimensions * dimensions / 2; i++) {
+            repeatImageUrls.push(imageUrls[i % imageUrls.length]);
+        }
+
+        imageUrls.push(...repeatImageUrls);
+    }
+
+    const items = pickRandomElements(imageUrls, dimensions * dimensions / 2);
+    const cards = items.concat(items);
+    const shuffledCards = shuffleArray(cards);
+
+    selectors.board.innerHTML = '';
+    const boardElement = document.createElement('div');
+    boardElement.classList.add('board');
+    boardElement.style.gridTemplateColumns = `repeat(${dimensions}, auto)`;
+
+    shuffledCards.forEach((cardPath) => {
+        const fileName = cardPath.match(/\/([^/]+)$/)[1];
+        const cardElement = document.createElement('div');
+        cardElement.classList.add('card');
+        
+        cardElement.innerHTML = `
+            <div class="card-front" style="background-image: url('${bgImage}');"></div>
+            <div class="card-back">
+                <img src="${cardPath}" alt="${fileName}" data-id="${fileName}">
+            </div>
+        `;
+        boardElement.appendChild(cardElement);
+    });
+
+    selectors.board.innerHTML = '';
+    selectors.board.appendChild(boardElement);
+};
+
+const startGame = () => {
+    state.gameStarted = true;
+    selectors.start.classList.add('disabled');
+    selectors.reset.classList.remove('disabled');
+    document.querySelectorAll('.card').forEach(card => {
+        card.classList.add('flipped');
+    });
+
+    setTimeout(() => {
+        document.querySelectorAll('.card').forEach(card => {
+            card.classList.remove('flipped');
+        });
+
+        state.loop = setInterval(() => {
+            state.totalTime++;
+            selectors.moves.innerText = `${state.totalFlips} moves`;
+            selectors.timer.innerText = `Time: ${state.totalTime} sec`;
+        }, 1000);
+    }, state.modeNum);
+};
+
+const resetGame = () => {
+    flipBackCards();
+    resetGameState();
+    selectors.start.classList.remove('disabled');
+    selectors.boardContainer.classList.remove('flipped');
+    selectors.reset.classList.add('disabled');
+    selectors.moves.innerText = '0 moves';
+    selectors.timer.innerText = 'Time: 0 sec';
+    setTimeout(() => {
+        setupGameBoard();
+    }, 500);
+};
+
+const flipBackCards = () => {
+    document.querySelectorAll('.card.flipped:not(.matched)').forEach(card => {
+        card.classList.remove('flipped');
+    });
+
+    state.flippedCards = 0;
+};
+
+const flipCard = card => {
+    if (!state.gameStarted) {
+        return;
+    }
+
+    state.flippedCards++;
+    
+    if (state.flippedCards <= 2) {
+        card.classList.add('flipped');
+    }
+    if (state.flippedCards === 2) {
+        state.totalFlips++;
+        setTimeout(() => {
+            const flippedCards = document.querySelectorAll('.flipped:not(.matched)');
+            if (flippedCards[0].querySelector('img').dataset.id === flippedCards[1].querySelector('img').dataset.id) {
+                flippedCards[0].classList.add('matched');
+                flippedCards[1].classList.add('matched');
+            }
+            flipBackCards();
+
+            if (!document.querySelectorAll('.card:not(.matched)').length) {
+                setTimeout(() => {
+                    selectors.boardContainer.classList.add('flipped');
+                    selectors.win.innerHTML = `
+                        <span class="win-text">
+                            You win!<br/>
+                            With <span class="highlight">${state.totalFlips}</span> moves<br/>
+                            Under <span class="highlight">${state.totalTime}</span> seconds
+                        </span>
+                    `;
+                    clearInterval(state.loop);
+                }, 500);
+            }
+        }, 1000);
+    }
+};
+
+const registerGameEventListeners = () => {
+    selectors.reset.classList.add('disabled');
+    let ismodeEnabled = false;
+    document.addEventListener('click', event => {
+        const eventTarget = event.target;
+        const eventParent = eventTarget.parentElement;
+        if(ismodeEnabled)
+            createRaindrop();
+
+        if (eventTarget.className.includes('card') && !eventParent.className.includes('flipped') && state.gameStarted) {
+            flipCard(eventParent);
+        } else if (eventTarget.className === 'start-btn' && !eventTarget.className.includes('disabled')) {
+            startGame();
+        } else if (eventTarget.className === 'reset-btn' && !eventTarget.className.includes('disabled')) {
+            resetGame();
+        } else if (eventTarget.className === 'mode-btn') {
+            ismodeEnabled = !ismodeEnabled;
+            // if(ismodeEnabled)
+            //     state.modeNum = 2000;
+            // else
+            //     state.modeNum = 0;
+            selectors.modeBtn.textContent = ismodeEnabled ? "T" : "F";
+        }
+    });
+
+    selectors.dimensionSelect.addEventListener('change', () => {
+        if (state.oldDimensions !== parseInt(selectors.dimensionSelect.value)) {
+            resetGame();
+        }
     });
 };
+
+setupGameBoard();
+registerGameEventListeners();
